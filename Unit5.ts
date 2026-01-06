eb679a10de5315938f7f7f7d48e22509e93e7e36
default <william.provost@stu.ocsb.ca>
2026-01-06 13:38:57 +0000

    added Unicode card icons and added pause function - which will make the code wait for enter to be pressed before proceeding to the game

--- Unit5.ts
+++ Unit5.ts
@@ -1,6 +1,17 @@
 /**
 * @author William Provost
-* @version 1.0.2
-* @date 2025-12-15
+* @version 1.0.0
+* @date 2026-01-06
 * @fileoverview Simplified number-based card game with Unicode cards
 */
+
+// Unicode card symbols (Ace–King of Spades)
+const cardSymbols: string[] = [
+  "🂡", "🂢", "🂣", "🂤", "🂥", "🂦", "🂧",
+  "🂨", "🂩", "🂪", "🂫", "🂭", "🂮"
+];
+
+// Pause function
+function pause(message: string): void {
+  prompt(message);
+}
