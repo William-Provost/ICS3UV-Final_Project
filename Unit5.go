7eeb628c9ac243ea5d413349fcd5936097004d6f
default <william.provost@stu.ocsb.ca>
2026-01-12 02:16:46 +0000

    fixed linting errors in certain comments

--- Unit5.go
+++ Unit5.go
@@ -108,7 +108,7 @@ func playRound(roundNum int) int {
 	for mini := 1; mini <= 3; mini++ {
 		result := playMiniRound(roundNum, mini)
 
-    // Program required switch here so there would be no linting errors
+  // Program required switch here so there would be no linting errors
 		switch result {
 case 1:
 			playerWins++
@@ -152,7 +152,7 @@ func main() {
 
 		winner := playRound(1)
 
-    // Program required switch here so there would be no linting errors
+  // Program required switch here so there would be no linting errors
 		switch winner {
 case 1:
 			fmt.Println("\nYou won the game!")
